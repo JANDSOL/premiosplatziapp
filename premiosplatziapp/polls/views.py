305131fe289184a42e8d2093abc5d56c274bc439
Juan Andrade <jpas.juan01@gmail.com>
2022-05-21 17:26:57 -0500

    :sparkles: [ADD] Add poll question view.

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,7 +1,19 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Question
+
 def index(request):
-    return HttpResponse("Hello, world!")
+    latest_question_list = Question.objects.all()
+    return render(request, "polls/index.html", {
+        "latest_question_list": latest_question_list
+    })
+
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta #{question_id}")
+
+def results(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta #{question_id}")
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando por la pregunta #{question_id}")
