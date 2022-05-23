e464ea9f0423d38104d2c10ba3dc407dd7eaafe0
Juan Andrade <jpas.juan01@gmail.com>
2022-05-23 10:13:27 -0500

    :hammer: [UPDATE] Update URL in index.html of the home polls template.

--- premiosplatziapp/polls/views.py
+++ premiosplatziapp/polls/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404
 
 from .models import Question
 
@@ -10,7 +11,10 @@ def index(request):
     })
 
 def detail(request, question_id):
-    return HttpResponse(f"Estás viendo la pregunta #{question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {
+        "question": question
+    })
 
 def results(request, question_id):
     return HttpResponse(f"Estás viendo los resultados de la pregunta #{question_id}")
